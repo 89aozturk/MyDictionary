6aa6eb3331c377194d351fd65325a31fff8da5eb
ALI OZTURK <aozturk1211@posta.pau.edu.tr>
2021-01-27 00:12:30 +0300

    Add project files.

---
+++ MyList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyList<Tkey,Tvalue>
+    {
+        Tkey[] _key;
+        Tvalue[] _value;
+        public MyList()
+        {
+            _key = new Tkey[0];
+            _value = new Tvalue[0];
+        }
+        public void Add(Tkey item1,Tvalue item2)
+        {
+            Tkey[] _tempkey = _key;
+            Tvalue[] _tempvalue = _value;
+            _key = new Tkey[_key.Length + 1];
+            _value = new Tvalue[_value.Length + 1];
+            for (int i = 0; i < _tempkey.Length; i++)
+            {
+                _key[i] = _tempkey[i];
+            }
+            _key[_key.Length - 1] = item1;
+
+            for (int j = 0; j < _tempvalue.Length; j++)
+            {
+                _value[j] = _tempvalue[j];
+            }
+            _value[_value.Length - 1] = item2;
+
+        }
+        public int Count
+        {
+            get { return _key.Length; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int, string> myList = new MyList<int, string>();
+            myList.Add(600, "Ali");
+            myList.Add(601, "Ayşe");
+            myList.Add(602,"Hakan");
+            myList.Add(603, "Asya");
+            myList.Add(604, "Fatma");
+
+            Console.WriteLine(myList.Count);
+        }
+    }
+}
